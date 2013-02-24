4e9e0772a6fabb11e698d8a24ef83c631359b82c
Peter Sunde <peter.sunde@gmail.com>
2013-02-24 21:57:03 +0100

    Update build script

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 @echo off
 
 set config=Release
-set target=bin
+set platform=AnyCpu
+set outputdir=%cwd%\bin
 set cwd=%CD%
-set commonflags=/p:DebugSymbols=false /p:DebugType=None
+set commonflags=/p:Configuration=%config%;Platform=%platform% /p:DebugSymbols=false /p:DebugType=None
 
 if not exist %fdir% (
     set fdir=%WINDIR%\Microsoft.NET\Framework
@@ -14,10 +15,10 @@ set msbuild=%fdir%\v4.0.30319\msbuild.exe
 :build
 echo ---------------------------------------------------------------------
 echo Compile started...
-%msbuild% src\Fleck2.csproj /p:Configuration=%config%;Platform=AnyCpu %commonflags% /tv:2.0 /p:TargetFrameworkVersion=v2.0 /p:OutputPath="%cwd%\bin\NET20"
-%msbuild% src\Fleck2.csproj /p:Configuration=%config%;Platform=AnyCpu %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:OutputPath="%cwd%\bin\NET35"
-%msbuild% src\Fleck2.csproj /p:Configuration=%config%;Platform=AnyCpu %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:OutputPath="%cwd%\bin\NET40"
-%msbuild% src\Fleck2.csproj /p:Configuration=%config%;Platform=AnyCpu %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:OutputPath="%cwd%\bin\NET45"
+%msbuild% src\Fleck2.csproj %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:OutputPath="%outputdir%\NET20"
+%msbuild% src\Fleck2.csproj %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:OutputPath="%outputdir%\NET35"
+%msbuild% src\Fleck2.csproj %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:OutputPath="%outputdir%\NET40"
+%msbuild% src\Fleck2.csproj %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:OutputPath="%outputdir%\NET45"
 
 :done
 echo.
