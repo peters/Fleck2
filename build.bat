6495b7a6631621d4f65748136f7e0f33445a420b
Peter Sunde <peter.sunde@gmail.com>
2013-02-24 22:08:51 +0100

    Fix wrong framework version

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ set msbuild=%fdir%\v4.0.30319\msbuild.exe
 :build
 echo ---------------------------------------------------------------------
 echo Compile started...
-%msbuild% src\Fleck2.csproj %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:OutputPath="%outputdir%\NET20"
+%msbuild% src\Fleck2.csproj %commonflags% /tv:2.0 /p:TargetFrameworkVersion=v2.0 /p:OutputPath="%outputdir%\NET20"
 %msbuild% src\Fleck2.csproj %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:OutputPath="%outputdir%\NET35"
 %msbuild% src\Fleck2.csproj %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:OutputPath="%outputdir%\NET40"
 %msbuild% src\Fleck2.csproj %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:OutputPath="%outputdir%\NET45"
