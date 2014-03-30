507038f1d0950b59d502a98ce1be767791a3386a
luckyrat <luckyrat@musites.com>
2014-03-30 19:00:33 +0100

    Support optional binding only to the loopback address

--- src/WebSocketServer.cs
+++ src/WebSocketServer.cs
@@ -13,15 +13,21 @@ namespace Fleck2
         private Action<IWebSocketConnection> _config;
 
         public WebSocketServer(string location)
-            : this(8181, location)
+            : this(8181, location, false)
         {
 
         }
 
         public WebSocketServer(int port, string location)
+            : this(8181, location, false)
+        {
+        }
+
+        public WebSocketServer(int port, string location, bool bindOnlyToLoopback)
         {
             var uri = new Uri(location);
             Port = uri.Port > 0 ? uri.Port : port;
+            BindOnlyToLoopback = bindOnlyToLoopback;
             Location = location;
             _scheme = uri.Scheme;
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
@@ -31,6 +37,7 @@ namespace Fleck2
         public ISocket ListenerSocket { get; set; }
         public string Location { get; private set; }
         public int Port { get; private set; }
+        public bool BindOnlyToLoopback { get; private set; }
         public X509Certificate2 Certificate { get; set; }
 
         public bool IsSecure
@@ -45,7 +52,7 @@ namespace Fleck2
 
         public void Start(Action<IWebSocketConnection> config)
         {
-            var ipLocal = new IPEndPoint(IPAddress.Any, Port);
+            var ipLocal = new IPEndPoint(BindOnlyToLoopback ? IPAddress.Loopback : IPAddress.Any, Port);
             ListenerSocket.Bind(ipLocal);
             ListenerSocket.Listen(100);
             FleckLog.Info("Server started at " + Location,null);
