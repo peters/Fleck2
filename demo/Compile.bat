f3d3f1201eaf2aca640f4c610a734a9fd6efbe0d
username1565 <39200328+username1565@users.noreply.github.com>
2019-10-23 00:08:04 +0300

    Fix pathway
    Fix pathway, because some files saved in "/bin/Releases". Now this saved in "/demo/bin/Releases", after compilation.

--- demo/Compile.bat
+++ demo/Compile.bat
@@ -3,5 +3,5 @@
 @echo off
 set fdir=%WINDIR%\Microsoft.NET\Framework
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
-%msbuild% /p:IntermediateOutputPath="../bin/Release/" /property:Configuration=Release Fleck2.Demo.csproj
+%msbuild% /p:IntermediateOutputPath="bin/Release/" /property:Configuration=Release Fleck2.Demo.csproj
 pause
